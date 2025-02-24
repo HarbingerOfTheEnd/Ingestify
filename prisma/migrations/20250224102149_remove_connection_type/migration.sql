f5d8ee5f07e9ff03cf17695b46d304c71ff668ae
Sai Kumar <ksai44576@gmail.com>
2025-02-24 15:52:21 +0530

    Remove connectionType column from EmailConfig model and migration

---
+++ prisma/migrations/20250224102149_remove_connection_type/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `connectionType` on the `EmailConfig` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EmailConfig" DROP COLUMN "connectionType";
